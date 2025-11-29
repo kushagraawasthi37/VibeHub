799e19225ed96913e07aade47467e1d01e3ecf76
Kushagraawasthi37@gmail.com <Kushagraawasthi37@gmailcom>
2025-11-30 03:23:14 +0530

    Some minor issue fixed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,7 +4,7 @@ import User from "../models/user.js";
 import Post from "../models/post.js";
 import Like from "../models/like.js";
 import Connection from "../models/connection.js";
-import Saved from "../models/Saved.js";
+import Saved from "../models/saved.js";
 import Share from "../models/share.js";
 import Comment from "../models/comment.js";
 
