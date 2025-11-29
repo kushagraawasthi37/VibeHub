63b8643cdc551a7ab4fbe7890c97e43753178c00
Kushagraawasthi37@gmail.com <Kushagraawasthi37@gmailcom>
2025-11-29 23:40:06 +0530

    Validate psot

--- backend/models/post.js
+++ backend/models/post.js
@@ -26,6 +26,20 @@ const postSchema = new mongoose.Schema({
     type: String,
     default: null,
   },
+
+  caption: {
+    type: String,
+    validate: {
+      validator: function (value) {
+        // Agar image ya video hai to caption zaroori
+        if (this.imageContent || this.videoContent) {
+          return !!value && value.trim().length > 0;
+        }
+        return true; // agar nahi hai to koi dikkat nahi
+      },
+      message: "Caption is required when image or video is present.",
+    },
+  },
 });
 
 const Post = mongoose.model("Post", postSchema);
